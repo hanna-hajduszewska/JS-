4977ff3edcff17ccccd875e0ab1b609945653ef7
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-11-02 11:09:52 +0100

    Create menu fade animation

--- Bankist/script.js
+++ Bankist/script.js
@@ -1,12 +1,17 @@
 'use strict';
 
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav');
+
+// Modal window
 const openModal = function (e) {
 	e.preventDefault();
 	modal.classList.remove('hidden');
@@ -50,10 +55,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 //Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
 	const clicked = e.target.closest('.operations__tab');
 
@@ -70,3 +71,21 @@ tabsContainer.addEventListener('click', function (e) {
 		.querySelector(`.operations__content--${clicked.dataset.tab}`)
 		.classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const handleHover = function (e) {
+	if (e.target.classList.contains('nav__link')) {
+		const link = e.target;
+		const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+		const logo = link.closest('.nav').querySelector('img');
+
+		siblings.forEach((el) => {
+			if (el !== link) el.style.opacity = this;
+		});
+		logo.style.opacity = this;
+	}
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
