2a3c41964dd0ba40c542f2c856ddfb10f9146128
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-11-27 17:27:54 +0100

    Implement search result

--- Forkify/forkify/src/js/model.js
+++ Forkify/forkify/src/js/model.js
@@ -4,11 +4,15 @@ import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
+  search: {
+    querry: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -23,6 +27,26 @@ export const loadRecipe = async function (id) {
     };
 
     console.log(state.recipe);
+  } catch (err) {
+    console.error(`${err}`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.querry = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
   } catch (err) {
     console.error(`${err}`);
     throw err;
