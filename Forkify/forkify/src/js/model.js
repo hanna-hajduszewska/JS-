6814b0ea16017fdd983f7783d7212b7cf9f41a47
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-11-27 15:27:03 +0100

    Create helpers and configuration files

--- Forkify/forkify/src/js/model.js
+++ Forkify/forkify/src/js/model.js
@@ -1,4 +1,6 @@
 // import 'regenerator-runtime/runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
@@ -6,15 +8,7 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    console.log(res, data);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -30,6 +24,6 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err}`);
   }
 };
